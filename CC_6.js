8344fd35f8f97ba3902309aa9e876e216ddd5ba5
Ingrid Rocha <ingrid22@usf.edu>
2025-10-05 19:09:58 -0400

    2nd commit with index.html

--- CC_6.js
+++ CC_6.js
@@ -19,3 +19,35 @@ class Manager extends Employee {
     return `${this.name} manages the ${this.department} department with a team of ${this.teamSize} people.`;
   }
 }
+
+
+class Company {
+  constructor() {
+    this.employees = [];
+  }
+
+  addEmployee(employee) {
+    this.employees.push(employee);
+  }
+
+  listEmployees() {
+    console.log("Employee List:");
+    this.employees.forEach(emp => {
+      console.log(emp.describe());
+    });
+  }
+}
+
+const company = new Company();
+
+const emp1 = new Employee("Alice", "Finance");
+const emp2 = new Employee("Jamal", "Marketing");
+const mgr1 = new Manager("Sophia", "Engineering", 10);
+const mgr2 = new Manager("Carlos", "Sales", 6);
+
+company.addEmployee(emp1);
+company.addEmployee(emp2);
+company.addEmployee(mgr1);
+company.addEmployee(mgr2);
+
+company.listEmployees();
