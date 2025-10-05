1f5e08abf276660d7b09937f2c06dc2b00c55d7a
Ingrid Rocha <ingrid22@usf.edu>
2025-10-05 19:03:50 -0400

    first commit

---
+++ CC_6.js
@@ -0,0 +1,21 @@
+class Employee {
+  constructor(name, department) {
+    this.name = name;
+    this.department = department;
+  }
+
+  describe() {
+    return `${this.name} works in the ${this.department} department.`;
+  }
+}
+
+class Manager extends Employee {
+  constructor(name, department, teamSize) {
+    super(name, department);
+    this.teamSize = teamSize;
+  }
+
+  describe() {
+    return `${this.name} manages the ${this.department} department with a team of ${this.teamSize} people.`;
+  }
+}
